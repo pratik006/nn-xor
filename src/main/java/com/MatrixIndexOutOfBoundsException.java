f25f881bb42c922aa1949396fbb21d7023d65827
Pratik Sengupta <pratik006@gmail.com>
2018-07-08 16:36:24 +0530

    working XOR

---
+++ src/main/java/com/MatrixIndexOutOfBoundsException.java
@@ -0,0 +1,18 @@
+package com;
+
+class MatrixIndexOutOfBoundsException extends RuntimeException
+{
+    public MatrixIndexOutOfBoundsException(Matrix matrix, int i, int j)
+    {
+        super("width = " + matrix.getHeight() + " height = " + matrix.getWidth() + " i = " + i + " j = " + j);
+    }
+
+    public MatrixIndexOutOfBoundsException()
+    {
+    }
+
+    public MatrixIndexOutOfBoundsException(String message)
+    {
+        super(message);
+    }
+}
--- src/main/java/com/NeuralNetwork.java
+++ src/main/java/com/NeuralNetwork.java
@@ -1,20 +1,21 @@
 package com;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
-import org.apache.commons.math3.linear.MatrixUtils;
-import org.apache.commons.math3.linear.RealMatrix;
 
 public class NeuralNetwork {
+	public static final int SCALE = 17;
+	public static final BigDecimal MINUS_ONE = new BigDecimal("-1");
+	
 	private int layers;
-	private int inputLen;
 	private int outputCount;
 	private int neuronCount;
-	double learningRate = 0.01d;
-	RealMatrix[] hiddenMatrix;
-	RealMatrix[] weightMatrices;
-	RealMatrix[] biasMatrices;
-	RealMatrix expectedMatrix;
+	BigDecimal learningRate = new BigDecimal("0.01");
+	BigDecimalMatrix[] hiddenMatrix;
+	BigDecimalMatrix[] weightMatrices;
+	BigDecimalMatrix[] biasMatrices;
+	BigDecimalMatrix expectedMatrix;
 	double avgErrorRate = 0;
 	long iterations;
 	
@@ -22,58 +23,57 @@ public class NeuralNetwork {
 		this.layers = layers;
 		this.outputCount = outputCount;
 		this.neuronCount = neuronCount;
-		initWeights2();
+		initWeights();
 	}
 	
 	public void initWeights2() {
-		double[][][] weights = new double[layers+1][neuronCount][];
-		double[][][] biases = new double[layers+1][][];
-		weights[0] = new double[][] {
-			{0.8476120393755635,-0.6731715020114386},
-	        {0.5716613629768057,0.10632328563113491},
-	          {0.5842254251316357,0.1262484323075692},
-	          {-0.9161971210157449,-0.24694179431984375}
+		String[][][] weights = new String[layers+1][neuronCount][];
+		String[][][] biases = new String[layers+1][][];
+		weights[0] = new String[][] {
+			{"0.8476120393755635","-0.6731715020114386"},
+	        {"0.5716613629768057","0.10632328563113491"},
+	          {"0.5842254251316357","0.1262484323075692"},
+	          {"-0.9161971210157449","-0.24694179431984375"}
 		};
 		
-		weights[1] = new double[][] {
-			{-0.9840260537481855,0.7193805539640015,-0.4294791687687227,-0.3465218493432385}
+		weights[1] = new String[][] {
+			{"-0.9840260537481855","0.7193805539640015","-0.4294791687687227","-0.3465218493432385"}
 		};
 		
-		biases[0] = new double[][] {
-			{0.5872951592253921},
-			{0.5606661418911605},
-			{0.4115604997168312},
-			{-0.6242288964207718}
+		biases[0] = new String[][] {
+			{"0.5872951592253921"},
+			{"0.5606661418911605"},
+			{"0.4115604997168312"},
+			{"-0.6242288964207718"}
 		};
-		biases[1] = new double[][] {{-0.6735499685229893}};
+		biases[1] = new String[][] {{"-0.6735499685229893"}};
 		
-		this.weightMatrices = new RealMatrix[2];
-		this.weightMatrices[0] = MatrixUtils.createRealMatrix(weights[0]);
-		this.weightMatrices[1] = MatrixUtils.createRealMatrix(weights[1]);
-		this.biasMatrices = new RealMatrix[2];
-		this.biasMatrices[0] = MatrixUtils.createRealMatrix(biases[0]);
-		this.biasMatrices[1] = MatrixUtils.createRealMatrix(biases[1]);
+		this.weightMatrices = new BigDecimalMatrix[2];
+		this.weightMatrices[0] = createRealMatrix(weights[0]);
+		this.weightMatrices[1] = createRealMatrix(weights[1]);
+		this.biasMatrices = new BigDecimalMatrix[2];
+		this.biasMatrices[0] = createRealMatrix(biases[0]);
+		this.biasMatrices[1] = createRealMatrix(biases[1]);
 		
-		hiddenMatrix = new RealMatrix[layers + 2];
+		hiddenMatrix = new BigDecimalMatrix[layers + 2];
 	}
 	
 	public void initWeights() {
-		double[][][] weights = new double[layers+1][neuronCount][];
-		double[][] biases = new double[layers+1][];
+		String[][][] weights = new String[layers+1][neuronCount][];
+		String[][] biases = new String[layers+1][];
 		
 		for (int i=0;i<=layers;i++) {
-			int l = (i==layers) ? outputCount : neuronCount;
-			biases[i] = new double[l];
+			int l = (i==layers) ? outputCount : (i==0)?2 : neuronCount;
 			for (int j=0;j<neuronCount;j++) {
-				weights[i][j] = new double[l];
+				weights[i][j] = new String[l];
 				for (int k=0; k<l; k++) {
-					weights[i][j][k] = Math.random();
+					weights[i][j][k] = Math.random()+"";
 				}
 			}
-			
-			for (int j=0;j<l;j++) {
-				biases[i][j] = Math.random();
-			}
+			int m = i==layers ? outputCount : neuronCount;
+			biases[i] = new String[m];
+			for (int j=0;j<m;j++)
+				biases[i][j] = Math.random()+"";
 		}
 		
 		/*weights = new double[][][] {
@@ -86,21 +86,26 @@ public class NeuralNetwork {
 			{ 0.45d, 0.45d },
 			{ 0.65d, 0.13d }
 		};*/
-		weightMatrices = new RealMatrix[weights.length];
-		biasMatrices = new RealMatrix[layers+1];
+		weightMatrices = new BigDecimalMatrix[weights.length];
+		biasMatrices = new BigDecimalMatrix[layers+1];
 		for (int i=0;i<weights.length;i++) {
-			weightMatrices[i] = MatrixUtils.createRealMatrix(weights[i]);
-			biasMatrices[i] = MatrixUtils.createColumnRealMatrix(biases[i]);
+			if (i==layers)
+				weightMatrices[i] = createRealMatrix(weights[i]).transpose();
+			else 
+				weightMatrices[i] = createRealMatrix(weights[i]);
 		}
 		
-		hiddenMatrix = new RealMatrix[layers + 2];
+		for (int i=0;i<biases.length;i++) {
+			biasMatrices[i] = createColumnRealMatrix(biases[i]);
+		}
+		
+		hiddenMatrix = new BigDecimalMatrix[layers + 2];
 	}
 	int iteration = 0;
 	public void train(double[] inputs, double[] expectedOutputs) {iteration++;
-		inputLen = inputs.length;
 		outputCount = expectedOutputs.length;
-		this.hiddenMatrix[0] = MatrixUtils.createColumnRealMatrix(inputs);
-		this.expectedMatrix = MatrixUtils.createColumnRealMatrix(expectedOutputs);
+		this.hiddenMatrix[0] = createColumnRealMatrix(inputs);
+		this.expectedMatrix = createColumnRealMatrix(expectedOutputs);
 		iterations++;
 		double[] res = forwardPass(1);
 		//System.out.println(Arrays.toString(inputs)+"  "+iteration+" "+Arrays.toString(res));
@@ -114,19 +119,18 @@ public class NeuralNetwork {
 	}
 	
 	public double[] predict(double[] inputs) {
-		inputLen = inputs.length;
-		this.hiddenMatrix[0] = MatrixUtils.createColumnRealMatrix(inputs);
+		this.hiddenMatrix[0] = createColumnRealMatrix(inputs);
 		iterations++;
 		return forwardPass(1);
 	}
 	
 	public double[] forwardPass(int layer) {
 		if (layer > layers + 1) {			
-			return hiddenMatrix[layer-1].getColumn(0);
+			return new double[] {hiddenMatrix[layer-1].getElement(1, 1).doubleValue()};
 		}
-		RealMatrix inputMatrix = hiddenMatrix[layer-1];
+		BigDecimalMatrix inputMatrix = hiddenMatrix[layer-1];
 		
-		RealMatrix weightMatrix = null;
+		BigDecimalMatrix weightMatrix = null;
 		/*if (inputMatrix.getColumn(0).length != neuronCount) {
 			weightMatrix = weightMatrices[layer-1].getSubMatrix(0, 3, 0, 1);
 		} else {*/
@@ -135,7 +139,7 @@ public class NeuralNetwork {
 		
 		
 		hiddenMatrix[layer] = weightMatrix.multiply(inputMatrix);
-		hiddenMatrix[layer] = hiddenMatrix[layer].add(biasMatrices[layer-1].getColumnMatrix(0));
+		hiddenMatrix[layer] = hiddenMatrix[layer].add(biasMatrices[layer-1]);
 		hiddenMatrix[layer] = sigmoid(hiddenMatrix[layer]);
 		return forwardPass(layer+1);
 	}
@@ -144,126 +148,148 @@ public class NeuralNetwork {
 		if (layer == 0)
 			return;
 		
-		RealMatrix outputs = hiddenMatrix[layer];
-		RealMatrix activations = hiddenMatrix[layer-1];
-		RealMatrix weights = weightMatrices[layer-1];
-		RealMatrix error = expectedMatrix.subtract(outputs);
+		BigDecimalMatrix outputs = hiddenMatrix[layer];
+		BigDecimalMatrix activations = hiddenMatrix[layer-1];
+		BigDecimalMatrix weights = weightMatrices[layer-1];
+		BigDecimalMatrix error = expectedMatrix.subtract(outputs);
 		
-		double[] outputErrors = error.getData()[0];
-		double[] gradients = Arrays.stream(outputErrors).map(NeuralNetwork::dsigmoid).toArray();
-		RealMatrix gradientMatrix = MatrixUtils.createColumnRealMatrix(scalarMultiply(gradients, outputErrors)).scalarMultiply(learningRate);
-		
-		//RealMatrix errorGradient = dsigmoid(outputs);
+		BigDecimalMatrix gradients = dsigmoid(outputs);
+		BigDecimalMatrix gradientMatrix = scalarMultiply(gradients, error).multiply(learningRate);
 		
 		// Calculate deltas
-		RealMatrix hiddenWeightErrors = gradientMatrix.multiply(activations.transpose());
+		BigDecimalMatrix hiddenWeightErrors = gradientMatrix.multiply(activations.transpose());
 		weightMatrices[layer-1] = weights.add(hiddenWeightErrors);
-		//todo 
 		biasMatrices[layer-1] = biasMatrices[layer-1].add(gradientMatrix);
 		calculateCost(layer - 1, error);
 	}
 	
-	private void calculateCost(int layer, RealMatrix error) {
+	private void calculateCost(int layer, BigDecimalMatrix error) {
 		if (layer == 0)
 			return;
 		
-		RealMatrix output = hiddenMatrix[layer];
-		RealMatrix activations = hiddenMatrix[layer-1];
-		RealMatrix doutput = dsigmoid(output);
-		RealMatrix weights = weightMatrices[layer];
+		BigDecimalMatrix output = hiddenMatrix[layer];
+		BigDecimalMatrix activations = hiddenMatrix[layer-1];
+		BigDecimalMatrix doutput = dsigmoid(output);
+		BigDecimalMatrix weights = weightMatrices[layer];
 		
-		RealMatrix hiddenErrors = weights.transpose().multiply(error);
+		BigDecimalMatrix hiddenErrors = (weights.transpose()).multiply(error);
 		//calc hidden gradient
 		//scalar multiply
-		RealMatrix hiddenGradient = scalarMultiply(doutput, hiddenErrors).scalarMultiply(learningRate);
+		BigDecimalMatrix hiddenGradient = scalarMultiply(doutput, hiddenErrors).multiply(learningRate);
 		
 		// calc deltas
-		RealMatrix weightErrors = hiddenGradient.multiply(activations.transpose());
+		BigDecimalMatrix weightErrors = hiddenGradient.multiply(activations.transpose());
 		//weightMatrices[layer-1] = scalarAdd(weightMatrices[layer-1], weightErrors);
 		weightMatrices[layer-1] = weightMatrices[layer-1].add(weightErrors);
 		biasMatrices[layer-1] = biasMatrices[layer-1].add(hiddenGradient);
 		calculateCost(layer - 1, hiddenErrors);
 	}
 	
-	private void print(RealMatrix matrix) {
+	private void print(BigDecimalMatrix matrix) {
 		System.out.println("[");
-		for (int i=0;i<matrix.getData().length;i++) {
-			for (int j=0;j<matrix.getData()[i].length;j++) {
-				System.out.print(matrix.getData()[i][j]+"    ");
+		for (int i=0;i<matrix.getHeight();i++) {
+			for (int j=0;j<matrix.getWidth();j++) {
+				System.out.print(matrix.getElement(i, j)+"    ");
 			}
 			System.out.println();
 		}
 		System.out.println("]");
 	}
 	
-	static double sigmoid(double val) {
-		return 1/(1+Math.exp(-val));
+	static BigDecimal sigmoid(BigDecimal val) {
+		return new BigDecimal(1/(1+Math.exp(-val.doubleValue())));
 	}
 	
-	static RealMatrix sigmoid(RealMatrix matrix) {
-		return MatrixUtils.createColumnRealMatrix(Arrays.stream(matrix.getColumn(0)).map(NeuralNetwork::sigmoid).toArray());
+	static BigDecimalMatrix sigmoid(BigDecimalMatrix matrix) {
+		BigDecimalMatrix result = new BigDecimalMatrix(matrix.getHeight(), matrix.getWidth(), 20);
+		for (int i=1;i<=matrix.getHeight();i++) {
+			for (int j=1;j<=matrix.getWidth();j++) {
+				result.setElement(i, j, sigmoid(matrix.getElement(i, j)));
+			}
+		}
+		return result;
 	}
 	
-	public static RealMatrix dsigmoid(RealMatrix matrix) {
-		return MatrixUtils.createColumnRealMatrix(Arrays.stream(matrix.getColumn(0)).map(val->val*(1 - val)).toArray());
+	public static BigDecimalMatrix dsigmoid(BigDecimalMatrix matrix) {
+		BigDecimalMatrix result = new BigDecimalMatrix(matrix.getHeight(), matrix.getWidth(), SCALE);
+		for (int i=1;i<=matrix.getHeight();i++) {
+			for (int j=1;j<=matrix.getWidth();j++) {
+				result.setElement(i, j, dsigmoid(matrix.getElement(i, j)));
+			}
+		}
+		return result;
 	}
 	
-	public static double dsigmoid(double y) {
-		return y*(1 - y);
+	public static BigDecimal dsigmoid(BigDecimal y) {
+		return y.multiply(BigDecimal.ONE.subtract(y));
 	}
 	
-	public static double[] scalarMultiply(double[] x, double[] y) {
-		double[] res = new double[x.length];
-		for (int i=0;i<x.length;i++) {
-			res[i] = x[i] * y[i];
+	public static BigDecimalMatrix scalarMultiply(BigDecimalMatrix x, BigDecimalMatrix y) {
+		BigDecimalMatrix res = new BigDecimalMatrix(x.getHeight(), x.getWidth(), 20);
+		for (int i=1;i<=x.getHeight();i++) {
+			for (int j=1;j<=x.getWidth();j++) {
+				res.setElement(i, j, x.getElement(i, j).multiply(y.getElement(i, j)));				
+			}
 		}
 		
 		return res;
 	}
 	
-	public static RealMatrix scalarMultiply(RealMatrix xm, RealMatrix ym) {
-		double[][] x = xm.getData();
-		double[][] y = ym.getData();
-		double[][] res = new double[x.length][x[0].length];
-		for (int i=0;i<x.length;i++) {
-			for (int j=0;j<x[i].length;j++) {
-				res[i][j] = x[i][j] * y[i][j];	
-			}
-		}
+	public static BigDecimalMatrix scalarAdd(BigDecimalMatrix xm, BigDecimalMatrix ym) {
+		BigDecimalMatrix r = new BigDecimalMatrix(xm.getRowDimension(), xm.getColumnDimension(), 20);
 		
-		return MatrixUtils.createRealMatrix(res);
-	}
-	
-	public static RealMatrix scalarAdd(RealMatrix xm, RealMatrix ym) {
-		double[][] x = xm.getData();
-		double[][] y = ym.getData();
-		
-		if (x.length == y.length && x[0].length == y[0].length) {
+		if (xm.getRowDimension() == ym.getRowDimension() && xm.getColumnDimension() == ym.getColumnDimension()) {
 			return xm.add(ym);
 		}
 		
-		double[][] res = new double[x.length][x[0].length];
+		/*double[][] res = new double[x.length][x[0].length];
 		for (int i=0;i<min(x.length, y.length);i++) {
 			for (int j=0;j<min(x[i].length, y[i].length);j++) {
 				res[i][j] = x[i][j] * y[i][j];
 			}
-		}
+		}*/
 		
-		return MatrixUtils.createRealMatrix(res);
+		return r;
 	}
 	
 	public static int min(int...vals) {
 		return Arrays.stream(vals).min().getAsInt();
 	}
 	
-	public static String getString(RealMatrix matrix) {
+	public static String getString(BigDecimalMatrix matrix) {
 		StringBuilder sb = new StringBuilder();
 		for (int i=0;i<matrix.getRowDimension();i++) {
 			for (int j=0;j<matrix.getColumnDimension();j++) {
-				sb.append(matrix.getData()[i][j]).append(",");
+				sb.append(matrix.getElement(i+1, j+1)).append(",");
 			}
 		}
 		
 		return sb.toString();
+	}
+	
+	public static BigDecimalMatrix createRealMatrix(String[][] data) {
+		BigDecimalMatrix matrix = new BigDecimalMatrix(data.length, data[0].length, SCALE);
+		for (int i=0;i<data.length;i++) {
+			for (int j=0;j<data[i].length;j++) {
+				matrix.setElement(i+1, j+1, new BigDecimal(data[i][j]));
+			}
+		}
+		return matrix;
+	}
+	
+	public static BigDecimalMatrix createColumnRealMatrix(String[] data) {
+		BigDecimalMatrix matrix = new BigDecimalMatrix(data.length, 1, SCALE);
+		for (int i=0;i<data.length;i++) {
+			matrix.setElement(i+1, 1, new BigDecimal(data[i]));
+		}
+		return matrix;
+	}
+	
+	public static BigDecimalMatrix createColumnRealMatrix(double[] data) {
+		BigDecimalMatrix matrix = new BigDecimalMatrix(data.length, 1, SCALE);
+		for (int i=0;i<data.length;i++) {
+			matrix.setElement(i+1, 1, new BigDecimal(data[i]));
+		}
+		return matrix;
 	}
 }
