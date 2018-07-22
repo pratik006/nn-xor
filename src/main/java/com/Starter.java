2cecd8e70c3201e07f56f4faca0fce9e79c2a8e9
Pratik Sengupta <pratik006@gmail.com>
2018-07-22 15:47:27 +0530

    send full array inputs

--- src/main/java/com/Starter.java
+++ src/main/java/com/Starter.java
@@ -1,25 +1,41 @@
 package com;
 
+import java.util.Arrays;
+
 import com.nn.dnn.NeuralNetwork;
 
 public class Starter {
 
 
 	public static void main(String[] args) {
-		double[][] inputs = new double[][]{
+		double[][] inTemplate = new double[][]{
 			{0, 0},
 			{0, 1},
 			{1, 0},
 			{1, 1}
 		};
-		double[][] outputs = new double[][]{
+		double[][] outTemplate = new double[][]{
 			{0},
 			{1},
 			{1},
 			{0}
 		};
+		
+		double[][] inputs = new double[10000][2];
+		double[][] outputs = new double[10000][2];
+		for (int k=0;k<10000;k++) {
+			int index = (int) ((Math.random()*10) % 4);
+			inputs[k] = inTemplate[index];
+			outputs[k] = outTemplate[index];
+		}
+		
 		NeuralNetwork nn = new NeuralNetwork();
 		nn.train(inputs, outputs);
+		
+		System.out.println("0 Result "+Arrays.toString(nn.predict(new double[] {1,1})));
+		System.out.println("1 Result "+Arrays.toString(nn.predict(new double[] {1,0})));
+		System.out.println("1 Result "+Arrays.toString(nn.predict(new double[] {0,1})));
+		System.out.println("0 Result "+Arrays.toString(nn.predict(new double[] {0, 0})));
 	}
 
 }
--- src/main/java/com/nn/dnn/NeuralNetwork.java
+++ src/main/java/com/nn/dnn/NeuralNetwork.java
@@ -3,37 +3,33 @@ package com.nn.dnn;
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
 
-import com.nn.activation.Sigmoid;
 import com.nn.activation.Tanh;
 import com.nn.layer.FullyConnected;
 import com.nn.layer.Layer;
 import com.nn.layer.OutputLayer;
 
 public class NeuralNetwork {
+	FullyConnected root = null;
 	
 	public void train(double[][] inputs, double[][] outputs) {
 		int nodes = 8;
-		FullyConnected root = new FullyConnected(2, nodes, Tanh.INSTANCE);
-		//FullyConnected layer1 = new FullyConnected(nodes, nodes, Sigmoid.INSTANCE);
+		root = new FullyConnected(2, nodes, Tanh.INSTANCE);
 		Layer layer2 = new OutputLayer(nodes, 1, Tanh.INSTANCE);
 		root.add(layer2);
-		//layer1.add(layer2);
 		
 		double avgLoss = 0;
-		for (int k=0;k<10000;k++) {
-			int index = (int) ((Math.random()*10) % 4);
-			RealMatrix expected = MatrixUtils.createColumnRealMatrix(outputs[index]);
-			RealMatrix input = MatrixUtils.createColumnRealMatrix(inputs[index]);
+		for (int i=0;i<inputs.length;i++) {
+			RealMatrix expected = MatrixUtils.createColumnRealMatrix(outputs[i]);
+			RealMatrix input = MatrixUtils.createColumnRealMatrix(inputs[i]);
 			
 			RealMatrix loss = root.forward(input, expected);
-			avgLoss = (k*avgLoss + loss.getEntry(0, 0))/(k+1);
+			avgLoss = (i*avgLoss + loss.getEntry(0, 0))/(i+1);
 			//System.out.println(input+"   "+loss+"    "+avgLoss);
 			root.backward(input, loss);			
 		}
-		
-		System.out.println("0 Result "+root.predict(MatrixUtils.createColumnRealMatrix(new double[] {1,1})));
-		System.out.println("1 Result "+root.predict(MatrixUtils.createColumnRealMatrix(new double[] {1,0})));
-		System.out.println("1 Result "+root.predict(MatrixUtils.createColumnRealMatrix(new double[] {0,1})));
-		System.out.println("0 Result "+root.predict(MatrixUtils.createColumnRealMatrix(new double[] {0,0})));
+	}
+	
+	public double[] predict(double[] input) {
+		return root.predict(MatrixUtils.createColumnRealMatrix(input)).getColumn(0);
 	}
 }
